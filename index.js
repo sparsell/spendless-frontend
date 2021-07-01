e9de96883c03639cdb67d17ebf25e11ed434f2ed
Sonja Parsell <hello@sparsell.com>
2021-07-01 16:57:34 -0400

    modal working

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const endPoint = "http://localhost:3000/api/v1/goals"
 
-document.addEventListener('DOMContentLoaded', () => getGoals())
+// document.addEventListener('DOMContentLoaded', () => getGoals())
 
-function getGoals() {
-    fetch(endPoint)
-        .then(response => response.json())
-        .then(goals => {
-            goals.data.forEach(goal => {
-                const goalCard = 
-            })
-        })
+
+// create modal
+
+let modalBtn = document.getElementById("modal-btn");
+let modal = document.getElementById("sl-modal")
+let closeBtn = document.getElementById("close-btn")
+
+modalBtn.onclick = function() {
+    modal.style.display = "block";
 }
+
+// modalBtn.addEventListener('click', function() {
+//     document.getElementById('modal').classList.add('is-visible');
+// });
+
+
+// function getGoals() {
+//     fetch(endPoint)
+//         .then(response => response.json())
+//         .then(goals => {
+//             goals.data.forEach(goal => {
+//                 const goalCard = 
+//             })
+//         })
+// }
 
 
 
