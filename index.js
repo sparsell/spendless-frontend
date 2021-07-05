9c19959da74b1572de950d9d812045c2129ca215
Sonja Parsell <hello@sparsell.com>
2021-07-05 16:49:56 -0400

    pseudo code flow of app

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const endPoint = "http://localhost:3000/api/v1/goals"
 
-// document.addEventListener('DOMContentLoaded', () => getGoals())
 
+// ***create modal***
+const modal = document.getElementById("sl-modal");
+const modalBtn = document.getElementById("modal-btn");
+const closeBtn = document.getElementById("delete");
+
+modalBtn.addEventListener('click', function() {
+    modal.style.display = 'block';
+});
+
+closeBtn.addEventListener('click', function() {
+    modal.style.display = 'none';
+});
+
+
+// ***Goal section***//
+// at open, ask for the user to set a goal:
+// if goal already set, display it (GET)
+
+// fetch goal (use IF statement:
+// IF goal.amount > 0, goal.amount
+// ELSE, ask user to set the amount)
+
+
+
+// document.addEventListener('DOMContentLoaded', () => getGoals())
 // function getGoals() {
 //     fetch(endPoint)
 //         .then(response => response.json())
@@ -13,17 +37,17 @@ const endPoint = "http://localhost:3000/api/v1/goals"
 // }
 
 
-// create modal
 
-let modalBtn = document.getElementById("modal-btn");
-let modal = document.getElementById("sl-modal");
-// let closeBtn = document.getElementById("close-btn");
-let closeBtn = document.getElementsByClassName("delete");
 
-modalBtn.addEventListener('click', function() {
-    modal.style.display = "block";
-});
+// ***Spend less amount section***//
 
-closeBtn.addEventListener('click', function() {
-    modal.style.display = "none";
-});
+// user enters a number in the first input (spendless_amounts.amount) and a brief description in the second input (need to add spendless_amounts.detail)
+// button "add to total" initiates fetch request to POST to db the amount and detail
+
+// also, adds the spendless_amount.amount to the total.total lol...
+
+// ***Total section***//
+
+// no input; updates automatically
+// 
+
