06d35c4e49bdeb18a23d3786d302a9dfc2b35461
Sonja Parsell <hello@sparsell.com>
2021-08-07 07:48:27 -0400

    created SpendlessAmount class

---
+++ spendlessAmounts..js
@@ -0,0 +1,16 @@
+class SpendlessAmount {
+    constructor(amount, description) {
+        this.amount = amount
+        this.description = description
+        }
+
+    showSpendlessAmount() {
+        return `
+        <div data-id=${this.id} class="card-content">
+            <h3> Amount: ${this.amount}</h3>
+            <h3> Description: ${this.description}</h3> 
+        </div>`
+    }    
+    }
+
+
