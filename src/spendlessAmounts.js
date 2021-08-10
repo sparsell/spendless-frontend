9bdf273f657504148117a070ae4920d12e22060c
Sonja Parsell <hello@sparsell.com>
2021-08-10 05:41:34 -0400

    debugging constructor

--- src/spendlessAmounts.js
+++ src/spendlessAmounts.js
@@ -1,19 +1,35 @@
 class SpendlessAmount {
-    constructor(id, SpendlessAmountAttributes) {
-        this.id = id
-        this.amount = SpendlessAmountAttributes.amount
-        this.description = SpendlessAmountAttributes.description
-        SpendlessAmount.call.push(this)
-        }
 
-    showSpendlessAmount() {
-        return `
-        <div data-id=${this.id} class="card-content">
-            <h3> Amount: ${this.amount}</h3>
-            <h3> Description: ${this.description}</h3> 
-        </div>
-        <br><br>`
-        }
+    constructor(data) {
+        debugger
+        this.id = data.id
+    }
+    // constructor(id, SpendlessAmountAttributes) {
+    //     this.id = id
+    //     this.amount = SpendlessAmountAttributes.amount
+    //     this.description = SpendlessAmountAttributes.description
+    //     SpendlessAmount.call.push(this)
+    //     }
+
+    // showSpendlessAmount() {
+    //     return `
+    //     <div data-id=${this.id} class="card-content">
+    //         <h3> Amount: ${this.amount}</h3>
+    //         <h3> Description: ${this.description}</h3> 
+    //     </div>
+    //     <br><br>`
+    //     }
+
+    // function renderSpendlessAmounts(spendless_amounts) {
+    //         let spendlessAmountsDiv = document.createElement('div')
+    //         spendlessAmountsDiv.classList.add("card")
+    //         let spendlessAmountsContent = document.createElement('div')
+    //         spendlessAmountsContent.classList.add("card-content")
+    //         let spendlessAmount = document.querySelector(".card-content")
+    //         spendlessAmountsDiv.innerHTML = `<h2 class="title is-4"> Amount: $${sl_amount.attributes.amount}  Description:${sl_amount.attributes.description}</h2>` 
+    //         spendlessAmount.appendChild(spendlessAmountsDiv)
+    //     }
+
     }
 
     SpendlessAmount.all = []
