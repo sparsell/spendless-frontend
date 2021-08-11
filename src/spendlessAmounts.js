730971db5ac96bb73481134428461d159267a288
sparsell <hello@sparsell.com>
2021-08-11 13:24:22 -0400

    building out class  constructor function

--- src/spendlessAmounts.js
+++ src/spendlessAmounts.js
@@ -1,35 +1,26 @@
 class SpendlessAmount {
 
-    constructor(data) {
-        debugger
-        this.id = data.id
+    constructor(sl_amount, sl_amountData) {
+        this.id = sl_amount.id
+        this.amount = sl_amountData.attributes.amount
+        this.description = sl_amountData.attributes.description
+        SpendlessAmount.all.push(this)
     }
-    // constructor(id, SpendlessAmountAttributes) {
-    //     this.id = id
-    //     this.amount = SpendlessAmountAttributes.amount
-    //     this.description = SpendlessAmountAttributes.description
-    //     SpendlessAmount.call.push(this)
-    //     }
 
-    // showSpendlessAmount() {
-    //     return `
-    //     <div data-id=${this.id} class="card-content">
-    //         <h3> Amount: ${this.amount}</h3>
-    //         <h3> Description: ${this.description}</h3> 
-    //     </div>
-    //     <br><br>`
-    //     }
+    // add each new spendless amount to the total
+    addToTotal(sl_amount) {
+        this.amount += total.total 
+    }
 
-    // function renderSpendlessAmounts(spendless_amounts) {
-    //         let spendlessAmountsDiv = document.createElement('div')
-    //         spendlessAmountsDiv.classList.add("card")
-    //         let spendlessAmountsContent = document.createElement('div')
-    //         spendlessAmountsContent.classList.add("card-content")
-    //         let spendlessAmount = document.querySelector(".card-content")
-    //         spendlessAmountsDiv.innerHTML = `<h2 class="title is-4"> Amount: $${sl_amount.attributes.amount}  Description:${sl_amount.attributes.description}</h2>` 
-    //         spendlessAmount.appendChild(spendlessAmountsDiv)
-    //     }
-
+    // add each new spendless amount to the table of all events
+    showSpendlessAmount(slAmountData) {
+        console.log(spendless_amount)
+        const newRow = document.createElement('tr')
+        const dateData = document.createElement('td')
+        const amountData = document.createElement('td')
+        const descriptionData = document.createElement('td')
+        console.log(newRow)
+            }
     }
 
     SpendlessAmount.all = []
