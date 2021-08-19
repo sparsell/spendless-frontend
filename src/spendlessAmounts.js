636eeee00e3d803601003fa96027deb2b0417ed8
sparsell <hello@sparsell.com>
2021-08-19 11:37:40 -0400

    show sl instance in row

--- src/spendlessAmounts.js
+++ src/spendlessAmounts.js
@@ -1,30 +1,40 @@
 class SpendlessAmount {
 
     constructor(sl_amountData) {
-        debugger
+        // debugger
+        // console.log(sl_amountData)
         this.id = sl_amountData.id
+        this.created_at = sl_amountData.attributes.created_at
         this.spendless_amount = sl_amountData.attributes.amount
         this.spendless_description = sl_amountData.attributes.description
         SpendlessAmount.all.push(this)
-        
+        // this.addToTotal()
+        this.showSpendlessAmount()
+      
     }
 
     // add each new spendless amount to the total
-    addToTotal(sl_amount) {
-        this.amount += total.total 
+    addToTotal() {
+        // debugger
+        // let currentTotal = document.querySelector(".sl-total").value 
+        // let newSlAmt = this.spendless_amount
+        // function calculateTotal(SpendlessAmount)
+        // currentTotal.innerText = newTotal
     }
 
     // add each new spendless amount to the table of all events
-    showSpendlessAmount(sl_amount) {
+    showSpendlessAmount() {
         // console.log(spendless_amount)
-        const table = document.querySelector(".sl-table")
-        const newRow = table.insertRow(0)
+        let date = new Date(this.created_at)
+        const table = document.querySelector("#sl-table")
+        const newRow = table.insertRow()
         const newDate = newRow.insertCell(0)
         const newAmount = newRow.insertCell(1)
         const newDescription = newRow.insertCell(2)
-        newDate.innerText = this.created_at
-        newAmount.innerText = this.amount
-        newDescription.innerText = this.description
+        newDate.innerHTML = `<td>${date}</td>`
+        newAmount.innerHTML = `<td><span>$</span>${this.spendless_amount}</td>`
+        newDescription.innerHTML = `<td>${this.spendless_description}</td>`
+          
             }
     }
 
